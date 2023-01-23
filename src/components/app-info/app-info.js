88ea5eba130a62cf0a91623086931d96ea30807a
Ilya Mukasei <ilya.mukosey@gmail.com>
2023-01-23 18:12:52 +0100

    React-crud-on-classes

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,15 @@
+import './app-info.css';
+
+const AppInfo = (props) => {
+    let {employees, increased} = props;
+    return (
+        <div className="app-info">
+            <h1>Учет сотрудников в компании</h1>
+            <h2>Общее число сотрудников: {employees}</h2>
+            <h2>Премию получат: {increased}</h2>
+        </div>
+    )
+}
+
+
+export default AppInfo;
